34d6413697f47211540bf7c931557060c156cf92
Marijn Haverbeke <marijnh@gmail.com>
2008-03-16 15:04:02 +0100

    forgot to add a new file in previous patch

---
+++ cl-postgres/sql-string.lisp
@@ -0,0 +1,39 @@
+(in-package :cl-postgres)
+
+(defun escape-bytes (bytes)
+  "Escape an array of octets in PostgreSQL's horribly inefficient
+textual format for binary data."
+  (let ((*print-pretty* nil))
+    (with-output-to-string (out)
+      (loop :for byte :of-type fixnum :across bytes
+            :do (if (or (< byte 32) (> byte 126) (= byte 39) (= byte 92))
+                    (progn
+                      (princ #\\ out)
+                      (princ (digit-char (ldb (byte 3 6) byte) 8) out)
+                      (princ (digit-char (ldb (byte 3 3) byte) 8) out)
+                      (princ (digit-char (ldb (byte 3 0) byte) 8) out))
+                    (princ (code-char byte) out))))))
+
+(defgeneric to-sql-string (arg)
+  (:documentation "Turn a lisp value into a string containing its SQL
+representation. Returns an optional second value that indicates
+whether the string should be escaped before being put into a query.")
+  (:method ((arg string))
+    (values arg t))
+  (:method ((arg vector))
+    (assert (typep arg '(vector (unsigned-byte 8))))
+    (values (escape-bytes arg) t))
+  (:method ((arg integer))
+    (princ-to-string arg))
+  (:method ((arg float))
+    (format nil "~f" arg))
+  (:method ((arg ratio))
+    (format nil "~f" arg))
+  (:method ((arg (eql t)))
+    "true")
+  (:method ((arg (eql nil)))
+    "false")
+  (:method ((arg (eql :null)))
+    "NULL")
+  (:method ((arg t))
+    (error "Value ~S can not be converted to an SQL literal." arg)))
