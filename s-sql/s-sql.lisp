3953c8fd24e913cceb80fa8eff81bd2ada987413
Marijn Haverbeke <marijnh@gmail.com>
2008-04-14 15:52:42 +0200

    replace dashes with spaces in symbols that are interpreted as type names, allowing stuff like timestamp-with-time-zone to work

--- s-sql/s-sql.lisp
+++ s-sql/s-sql.lisp
@@ -189,7 +189,7 @@ something SQL understands. Default is to just use the type symbol's
 name.")
   (:method ((lisp-type symbol) &rest args)
     (declare (ignore args))
-    (symbol-name lisp-type))
+    (map 'string (lambda (ch) (if (eq ch #\-) #\space ch)) (symbol-name lisp-type)))
   (:method ((lisp-type (eql 'string)) &rest args)
     (cond (args (format nil "CHAR(~A)" (car args)))
           (t "TEXT")))
