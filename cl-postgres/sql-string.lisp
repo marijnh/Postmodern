4da248dce061779af2de9d27658feea38aeecdb9
attila.lendvai <attila.lendvai@gmail.com>
2009-02-06 12:24:12 +0100

    added a write-rational-as-foating-point function and use it to serialize rationals when sending them to Postgres.
    Ignore-this: 45c9cc579792d173983382218dbfbac1
    
    it limits their length at 38 digits and throws an error if they don't fit (instead of silently losing precision as it did before).
    
    with this change storing 0.7 into a NUMERIC column will properly read back as 0.7.

--- cl-postgres/sql-string.lisp
+++ cl-postgres/sql-string.lisp
@@ -14,6 +14,37 @@ textual format for binary data."
                       (princ (digit-char (ldb (byte 3 0) byte) 8) out))
                     (princ (code-char byte) out))))))
 
+(defun write-rational-as-foating-point (input-number stream digit-length-limit)
+  (declare (optimize speed))
+  (flet ((fail ()
+           (error "Can not write the rational ~F with only ~A digits" (coerce input-number 'double-float) digit-length-limit)))
+    (declare (inline fail))
+    (let ((number input-number))
+      (when (< number 0)
+        (write-char #\- stream)
+        (setf number (abs number)))
+      (multiple-value-bind (quotient remainder) (floor number)
+        (let* ((quotient-part (with-output-to-string (stream)
+                                (write quotient :stream stream)))
+               (quotient-digit-length (length quotient-part))
+               (decimal-length-limit (- digit-length-limit quotient-digit-length)))
+          (when (<= decimal-length-limit 0)
+            (fail))
+          (write-string quotient-part stream)
+          (write-char #\. stream)
+          (setf number remainder)
+          (loop
+             :for decimal-digits :upfrom 1
+             :until (zerop number)
+             :do (progn
+                   (when (> decimal-digits decimal-length-limit)
+                     (fail))
+                   ;; (format *debug-io* "*** step ~A, number is ~A~%" decimal-digits number)
+                   (setf number (* number 10))
+                   (multiple-value-bind (quotient remainder) (floor number)
+                     (write quotient :stream stream)
+                     (setf number remainder)))))))))
+
 (defgeneric to-sql-string (arg)
   (:documentation "Turn a lisp value into a string containing its SQL
 representation. Returns an optional second value that indicates
@@ -28,9 +59,14 @@ whether the string should be escaped before being put into a query.")
   (:method ((arg float))
     (format nil "~f" arg))
   (:method ((arg ratio))
-    ;; There's no support for rationals in PostreSQL,
-    ;; so do the best we can: format it as a double-float.
-    (format nil "~,,,,,,'EE" (coerce arg 'double-float)))
+    ;; Possible optimization: we could probably build up the same binary structure postgres
+    ;; sends us instead of sending it as a string. See the "numeric" interpreter for more details...
+    (with-output-to-string (result)
+      ;; PostgreSQL happily handles 200+ decimal digits, but the SQL standard only requires
+      ;; 38 digits from the NUMERIC type, and Oracle also doesn't handle more. For practical
+      ;; reasons we also draw the line there. If someone needs full rational numbers then
+      ;; 200 wouldn't help them much more than 38...
+      (write-rational-as-foating-point arg result 38)))
   (:method ((arg (eql t)))
     "true")
   (:method ((arg (eql nil)))
