7e1a14356146b323dbabd1905785c4049180f3e8
Marijn Haverbeke <marijnh@gmail.com>
2011-10-14 10:57:29 +0200

    Fix some oversights in previous patch

--- cl-postgres/package.lisp
+++ cl-postgres/package.lisp
@@ -7,7 +7,7 @@
            #:database-error-detail
            #:database-error-query
            #:database-error-cause
-           #:database-error-contraint-name
+           #:database-error-constraint-name
            #:database-connection
            #:database-connection-error
            #:database-socket-error
