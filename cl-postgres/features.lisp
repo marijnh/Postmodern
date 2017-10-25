99c14dac6c4d309d59e320cb2d054a4f5c8513c2
Benjamin Vulpes <benkay@gmail.com>
2017-10-25 14:16:25 -0700

    set features when executing as well

--- cl-postgres/features.lisp
+++ cl-postgres/features.lisp
@@ -5,7 +5,7 @@
 (in-package :cl-postgres.features)
 
 
-(eval-when (:compile-toplevel :load-toplevel)
+(eval-when (:compile-toplevel :load-toplevel :execute)
   (when (find-package 'sb-bsd-sockets)
     (pushnew 'sbcl-available *features*)
 
