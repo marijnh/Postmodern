a2617d01601185c2018724349514f177325ed51a
Sabra Crolleton <sabra.crolleton@gmail.com>
2024-07-11 13:06:55 -0400

    Minor formatting change

--- postmodern/query.lisp
+++ postmodern/query.lisp
@@ -165,11 +165,11 @@ such as with updating or deleting queries, this is returned as a second value."
                      :else :collect arg)))
     (destructuring-bind (reader result-form) (reader-for-format format)
       (let ((base (if args
-		                  (let ((vars (loop :for x :in args :collect (gensym))))
-			                  `(let ,(loop :for v :in vars :for a :in args :collect `(,v ,a))
-			                     (prepare-query *database* "" ,(real-query query) (list ,@vars))
-			                     (exec-prepared *database* "" (list ,@vars) ,reader)))
-		                  `(exec-query *database* ,(real-query query) ,reader))))
+		      (let ((vars (loop :for x :in args :collect (gensym))))
+			`(let ,(loop :for v :in vars :for a :in args :collect `(,v ,a))
+			   (prepare-query *database* "" ,(real-query query) (list ,@vars))
+			   (exec-prepared *database* "" (list ,@vars) ,reader)))
+		      `(exec-query *database* ,(real-query query) ,reader))))
         `(,result-form ,base)))))
 
 (defmacro execute (query &rest args)
